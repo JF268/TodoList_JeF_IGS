4d8c0f507c8edaee0c2119c4b08f819949dd7dc1
franco <201913375g@utea.edu.pe>
2022-10-12 01:41:49 -0500

    creando la conexión con mysql - xampp

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrate en mi ToDoList</title>
+    
+</head>
+<body>
+    <form method="post">
+        <h1> Registrate para administrar tus tareas </h1>
+        <input type="text" name="name" placeholder="Nombre Completo">
+        <input type="email" name="email" placeholder="Emial">
+        <input type="submit" name="register">
+    </form>
+</body>
+</html>
