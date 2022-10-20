a857b6dbeb902f564b34d2f152c9ea4bb9e4395c
franco <201913375g@utea.edu.pe>
2022-10-20 00:57:49 -0500

    primera parte de validación de campos en registrar

--- vista/home/registrar.php
+++ vista/home/registrar.php
@@ -16,13 +16,13 @@
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Correo Electrónico</label>
             <!--- correo-electrónico -->
-            <input type="email" name="correo" class="form-control" placeholder="correo@example.com" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input type="email" name="correo" value="<?=(!empty($_GET['correo']))? $_GET['correo']:"" ?>" class="form-control" placeholder="correo@example.com" id="exampleInputEmail1" aria-describedby="emailHelp">
         </div>
         <!-- nombre--->
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Nombre Completo</label>
             <!--- name -->
-            <input type="text" name="named2" class="form-control" placeholder="Escriba su nombre completo" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input type="text" name="named2" value="<?=(!empty($_GET['named2']))? $_GET['named2']:"" ?>" class="form-control" placeholder="Escriba su nombre completo" id="exampleInputEmail1" aria-describedby="emailHelp">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Contraseña</label>
@@ -46,6 +46,13 @@
             <!--- repetir contraseña ---->
             <input type="password" name="confirmarContraseña" class="form-control" id="password2">
         </div>
+        <!-- apartado para mandar mensaje de error en el archivo store con sentencia if en php--->
+        <?php if(!empty($_GET['error'])):?>
+            <div id="alerta" style="margin:auto;" class="alert alert-danger mb-2" role="alert">
+                <?= !empty($_GET['error'])? $_GET['error']:"" ?>
+            </div>
+        <?php endif;?>
+        
         <div class="d-grid gap-2">
             <!--- boton submit---->
             <button type="submit" name="submit" class="btn btn-primary">CREAR CUENTA</button>
--- vista/home/store.php
+++ vista/home/store.php
@@ -1,10 +1,16 @@
 <?php
     require_once("C://xampp/htdocs/ToDoList_JF_IGS/controlador/homeController.php");
     $obj = new homeController();
-    echo $correo = $_POST['correo'];
-    echo $named2 = $_POST['named2'];
-    echo $contraseña = $_POST['contraseña'];
-    echo $confirmarContraseña = $_POST['confirmarContraseña'];
+    $correo = $_POST['correo'];
+    $named2 = $_POST['named2'];
+    $contraseña = $_POST['contraseña'];
+    $confirmarContraseña = $_POST['confirmarContraseña'];
 
-    
+    $error ="";
+    // estructura de validación mediante sentencia if, verificando si los campos estan vacios
+    if(empty($correo) || empty($named2) || empty($contraseña) || empty($confirmarContraseña)){
+        $error .= "<li>Completa los campos</li>"; // nos imprime un listado para llenar los campos
+        header("Location: registrar.php?error=".$error."&&correo=".$correo."&&named2".$named2."&&contraseña".$contraseña."&&confirmarContraseña".$confirmarContraseña); // propiedad header para redirigirnos al formulario de registro mediante el metodo GET
+    }
+
 ?>
